399278e13e7c669a2a05f1e9d032c93f3a266e74
David Prause <dprause@jaystargroup.com>
2013-06-17 14:13:22 -0400

    initial commit of project

---
+++ ProductStoreEF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductStoreEF.MvcApplication" Language="C#" %>
---
+++ ProductStoreEF/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductStoreEF.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Customer { get; set; }
+
+        // Navigation property
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
